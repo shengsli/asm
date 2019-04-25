1202ad4c75347c3e6f1f1c9888680346cc86a78c
shengsli <sheng.s.li@outlook.com>
2019-04-25 09:56:42 +0100

    pt3

--- compl.sh
+++ compl.sh
@@ -2,3 +2,6 @@
 ASM_NAME="$1"
 nasm -f elf32 $ASM_NAME.asm -o $ASM_NAME.o
 ld -m elf_i386 $ASM_NAME.o -o $ASM_NAME
+./$ASM_NAME
+echo ''
+#echo $?
--- ex5.asm
+++ ex5.asm
@@ -1,10 +1,14 @@
-	global _start
-	section .data
+global _start
+section .data
 	addr db "yellow"
-	section .text
+section .text
 _start:
 	mov eax, 4
 	mov ebx, 1
-	mov ecx addr
+	mov ecx, addr
 	mov edx, 6
 	int 0x80
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
+	
